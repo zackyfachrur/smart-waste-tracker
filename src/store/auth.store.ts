df76b578e6e035d14dda737b5b2cbf9f8eec1d15
zackyfachrur <mochzackyaja@gmail.com>
2026-05-09 10:40:57 +0700

    fix: fix protected route

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -28,7 +28,6 @@ export const useAuthStore = create<AuthState>((set) => ({
 
         set({ user_id: null, token: null, role_id: null, name: null });
 
-        window.location.href = "/mobile/authentication";
     },
 
     initAuth: () => {
